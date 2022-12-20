64c1521f32396ca257ba2c9f2133a20671e14965
Soufiane <sammahsoufiane01@gmail.com>
2022-12-20 14:45:06 +0100

    adding sticky nav

--- js/script.js
+++ js/script.js
@@ -3,13 +3,14 @@
 // Opening and closing the mobile navigation
 
 const headerEl = document.querySelector(".header");
-const bntEl = document.querySelector(".btn-mobile-nav");
+const MobilebntEl = document.querySelector(".btn-mobile-nav");
 
-bntEl.addEventListener("click", () => {
+MobilebntEl.addEventListener("click", () => {
   headerEl.classList.toggle("nav-open");
+  document.body.classList.toggle("fixed-body");
 });
 
-//Smooth scrolling
+//*********************Smooth scrolling***************
 
 //helper function
 const ScrollElIntoView = El => {
@@ -24,6 +25,12 @@ const navListEl = document.querySelector(".main-nav-list");
 navListEl.addEventListener("click", e => {
   e.preventDefault();
 
+  //if mobile menu is open close it , and enable scrolling
+  if (headerEl.classList.contains("nav-open")) {
+    headerEl.classList.remove("nav-open");
+    document.body.classList.remove("fixed-body");
+  }
+  /****************************************** */
   // get the href to use it as an id
   const ELhref = e.target.getAttribute("href");
 
@@ -61,3 +68,24 @@ logos.forEach(logo => {
     );
   });
 });
+
+//*********************Sticky Navigation***************
+
+const NavObsererCallback = (entries, _) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    headerEl.classList.add("sticky-header");
+  } else headerEl.classList.remove("sticky-header");
+};
+const NavObsererOptions = {
+  root: null,
+  threshold: [0],
+};
+
+const NavObserer = new IntersectionObserver(
+  NavObsererCallback,
+  NavObsererOptions
+);
+
+NavObserer.observe(document.querySelector(".section-hero"));
